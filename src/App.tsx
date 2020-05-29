252dc7054a932a858c64ff9034b3468bd020f530
Victor Hermes <me.victorhermes@gmail.com>
2020-05-29 10:02:48 -0300

    global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Routes from './routes';
 import { BrowserRouter } from 'react-router-dom';
+import GlobalStyle from './styles/global';
 
 const App = () => (
-    <BrowserRouter>
-        <Routes />
-    </BrowserRouter>
+    <>
+        <BrowserRouter>
+            <Routes />
+        </BrowserRouter>
+        <GlobalStyle />
+    </>
 );
 
 export default App;
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Title } from './styles';
 
 const Dashboard: React.FC = () => {
-    return <h1>Dashboard</h1>;
+    return <Title>Dashboard</Title>;
 };
 
 export default Dashboard;
