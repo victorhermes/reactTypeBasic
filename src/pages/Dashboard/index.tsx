7a685bd6c9d42993e58e6a2d97d1c76027ead838
Victor Hermes <me.victorhermes@gmail.com>
2020-06-01 20:31:33 -0300

    dashboard style

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,90 @@
 import React from 'react';
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+import { FiChevronRight } from 'react-icons/fi';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-    return <Title>Dashboard</Title>;
+    return (
+        <>
+            <img src={logoImg} alt="Logo" />
+            <Title>Dashboard</Title>
+
+            <Form>
+                <input type="text" placeholder="Digite o nome do repositório" />
+                <button type="submit">Pesquisar</button>
+            </Form>
+
+            <Repositories>
+                <a href="teste">
+                    <img
+                        src="https://avatars3.githubusercontent.com/u/25070025?s=400&u=9644db2c8ca12aaed6a2276aa4279c9745f28cc6&v=4"
+                        alt="Perfil"
+                    />
+
+                    <div>
+                        <strong>victorhermes/teste</strong>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit.
+                        </p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+
+                <a href="teste">
+                    <img
+                        src="https://avatars3.githubusercontent.com/u/25070025?s=400&u=9644db2c8ca12aaed6a2276aa4279c9745f28cc6&v=4"
+                        alt="Perfil"
+                    />
+
+                    <div>
+                        <strong>victorhermes/teste</strong>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit.
+                        </p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+
+                <a href="teste">
+                    <img
+                        src="https://avatars3.githubusercontent.com/u/25070025?s=400&u=9644db2c8ca12aaed6a2276aa4279c9745f28cc6&v=4"
+                        alt="Perfil"
+                    />
+
+                    <div>
+                        <strong>victorhermes/teste</strong>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit.
+                        </p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+
+                <a href="teste">
+                    <img
+                        src="https://avatars3.githubusercontent.com/u/25070025?s=400&u=9644db2c8ca12aaed6a2276aa4279c9745f28cc6&v=4"
+                        alt="Perfil"
+                    />
+
+                    <div>
+                        <strong>victorhermes/teste</strong>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit.
+                        </p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+            </Repositories>
+        </>
+    );
 };
 
 export default Dashboard;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,96 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 export const Title = styled.h1`
     font-size: 48px;
     color: grey;
+    max-width: 450px;
+    margin-top: 80px;
+    line-height: 56px;
+`;
+
+export const Form = styled.form`
+    margin-top: 40px;
+    max-width: 700px;
+    display: flex;
+
+    input {
+        flex: 1;
+        height: 70px;
+        padding: 0 24px;
+        border: 0;
+        border-radius: 5px 0 0 5px;
+        color: #a8a8b3;
+
+        &::placeholder {
+            color: #a8a8b3;
+        }
+    }
+
+    button {
+        width: 210px;
+        background: #04d363;
+        border-radius: 0 5px 5px 0;
+        border: 0;
+        color: #ffffff;
+        font-weight: bold;
+        transition: background-color 0.2s;
+
+        &:hover {
+            background: ${shade(0.2, '#04d363')};
+        }
+    }
+`;
+
+export const Repositories = styled.div`
+    margin-top: 80px;
+    max-width: 700px;
+
+    a {
+        background: #ffffff;
+        border-radius: 5px;
+        width: 100%;
+        padding: 24px;
+        display: block;
+        text-decoration: none;
+
+        display: flex;
+        align-items: center;
+
+        & + a {
+            margin-top: 10px;
+        }
+
+        transition: transform 0.2s;
+
+        &:hover {
+            transform: translateX(10px);
+        }
+
+        img {
+            width: 64px;
+            height: 64px;
+            border-radius: 50%;
+        }
+
+        div {
+            margin-left: 16px;
+
+            strong {
+                font-size: 20px;
+                color: #3d3d3d;
+            }
+
+            p {
+                font-size: 18px;
+                color: #a8a8b3;
+                margin-top: 4px;
+            }
+        }
+    }
+
+    svg {
+        margin-left: auto;
+        color: #cbcbd6;
+    }
 `;
